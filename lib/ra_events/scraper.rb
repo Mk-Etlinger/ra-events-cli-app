ee499b6a8afc9e192141c719ab5ee40f7a7868db
ETphonehomeslice <et88guy@hotmail.com>
2017-04-02 18:40:22 -0400

    added spec.md and a note in scraper.rb

--- lib/ra_events/scraper.rb
+++ lib/ra_events/scraper.rb
@@ -26,19 +26,30 @@ class Scraper
     ra_events_page = Nokogiri::HTML(open("https://www.residentadvisor.net/events.aspx"))
 
     ra_events_page.css(".col4").each do |e|
-      puts "#{e.css('a').first.text}"
-      # binding.pry
-      e.css('.bbox').each do |e|
-        puts "Title: #{e.css('a').text}"
-        puts "Location: #{e.css('span').text}"
-        puts "Artists: #{e.css('div').text}" if !e.css('div').text.empty?
-        puts "Url: #{e.css('a').attribute('href').text}"
-
-
-        binding.pry
-      end
+      puts "#{e.css(".date").first.text}
+      ***********************
+      ******************
+      ***********
+      *******
+      ***
+      *"
+      binding.pry
+      # e.css('.bbox').each do |e|
+      #   puts "Title: #{e.css('a').text}"
+      #   puts "Location: #{e.css('span').text}"
+      #   puts "Artists: #{e.css('div').text}" if !e.css('div').text.empty?
+      #   puts "Url: #{e.css('a').attribute('href').text}"
+      #   puts ""
+      # end
     
     end
   end
 
 end
+
+
+#This will grab the Date of all of the listed shows
+
+# ra_events_page.css("#event-listing .date").each do |e|
+#   puts "#{e.css('a').first.text}"
+# end
