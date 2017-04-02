21c36c453e3e9d8859d11d54880144fb41a74658
ETphonehomeslice <et88guy@hotmail.com>
2017-04-01 23:18:07 -0400

    Scraper is now listing each event: Title, Location, Artists and URL

--- lib/ra_events/scraper.rb
+++ lib/ra_events/scraper.rb
@@ -1,3 +1,7 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
 class Scraper
 
   attr_accessor 
@@ -6,5 +10,35 @@ class Scraper
 
   end
   
+  def self.scrape_links(index_url)
+    states_with_link = {}
+    ra_events_page = Nokogiri::HTML(open("https://www.residentadvisor.net/events.aspx"))
+
+    ra_events_page.css("li.parent a").each do |e|
+      states_with_link[e.children.text] = e['href'] unless e.children.text == "Washington DC"
+    end
+
+    states_with_link
+  end
+
+  def self.scrape_events_page(url)
+
+    ra_events_page = Nokogiri::HTML(open("https://www.residentadvisor.net/events.aspx"))
+
+    ra_events_page.css(".col4").each do |e|
+      puts "#{e.css('a').first.text}"
+      # binding.pry
+      e.css('.bbox').each do |e|
+        puts "Title: #{e.css('a').text}"
+        puts "Location: #{e.css('span').text}"
+        puts "Artists: #{e.css('div').text}" if !e.css('div').text.empty?
+        puts "Url: #{e.css('a').attribute('href').text}"
+
+
+        binding.pry
+      end
+    
+    end
+  end
 
 end
