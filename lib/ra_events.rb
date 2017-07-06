0f52c95a5da093a08ee2d1167a4b2366670f1316
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-06 10:24:28 -0400

    properly namespace gem with require

--- lib/ra_events.rb
+++ lib/ra_events.rb
@@ -1,4 +1,6 @@
-require_relative "./ra_events/version"
-require_relative './ra_events/cli'
-require_relative './ra_events/scraper'
-require_relative './ra_events/event'
+module RaEvents
+  require "ra_events/version"
+  require 'ra_events/cli'
+  require 'ra_events/scraper'
+  require 'ra_events/event'
+end
